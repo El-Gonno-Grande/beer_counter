cd4009e1afd0029db7ef9a7ef70eb7e7705ba4f9
kollerlukas <lukaskoller6@gmail.com>
2020-08-16 13:28:37 +0200

    nicer map + beer pin

--- lib/beermap.dart
+++ lib/beermap.dart
@@ -29,25 +29,31 @@ class _BeerMapState extends State<BeerMapPage> {
       ),
       body: FlutterMap(
         options: MapOptions(
+          minZoom: 1.0,
+          maxZoom: 18.0,
           center: latestBeer.getLocation(),
           zoom: 13.0,
         ),
         layers: [
           TileLayerOptions(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              urlTemplate:
+                  'https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}{r}.png?apikey=1ccbe9a0e02a437b89226c3344e6c3cf',
               subdomains: ['a', 'b', 'c']),
           MarkerLayerOptions(
             markers: widget.beers
                 .map((e) => Marker(
                       width: 80.0,
                       height: 80.0,
+                      anchorPos: AnchorPos.exactly(Anchor(40.0, 0.0)),
                       point: e.getLocation(),
                       builder: (ctx) => Container(
-                        child: SvgPicture.asset(
-                          'assets/beer_location.svg',
-                          height: 50.0,
-                          width: 50.0,
-                        ),
+                        child:
+                            /*SvgPicture.asset(
+                          'assets/beer_pin.svg',
+                          height: 432.0, // 50.0
+                          width: 432.0, // 50.0
+                        ),*/
+                            Image.asset('assets/beer_pin.png'),
                       ),
                     ))
                 .toList(),
